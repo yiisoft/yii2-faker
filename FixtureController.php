759ade36d01728a2e687312d9fdbbe58cf0f09a1
Skiba Max <m.skiba@favbet.com>
2017-02-10 12:21:43 +0200

    findTemplateFiles: strip dot-slash for templates under templatePath

--- FixtureController.php
+++ FixtureController.php
@@ -367,7 +367,8 @@ class FixtureController extends \yii\console\controllers\FixtureController
 
         foreach ($files as $fileName) {
             $relativeName = str_replace(Yii::getAlias($this->templatePath) . '/', "", $fileName);   // strip templatePath from current template's full path
-            $relativeName = dirname($relativeName) . '/' . basename($relativeName,'.php');          // strip extension
+            $relativeDir = dirname($relativeName) == '.' ? '' : dirname($relativeName) . '/';
+            $relativeName = $relativeDir . basename($relativeName,'.php');          // strip extension
             $foundTemplates[] = $relativeName;
         }
 
