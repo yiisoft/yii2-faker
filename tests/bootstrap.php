eead2e5e109a83466e84b3d27a0d3e8ff4b5b331
Alexander Makarov <sam@rmcreative.ru>
2017-04-19 17:31:01 +0300

    Fixes phpunit 6 compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/faker', __DIR__);
 Yii::setAlias('@yii/faker', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
